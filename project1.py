e8427b05fb606bcb11dee69cf4c0d5f40f92c568
Alexander Schwartzberg <aeksco@gmail.com>
2018-03-29 20:58:41 -0400

    Fixed round() function calls to 1 decimal place

--- project1.py
+++ project1.py
@@ -337,9 +337,9 @@ def first_come_first_served(processes, t_cs):
     # Puts statistics into result dict
     result = newResult()
     result['name'] = 'FCFS'
-    result['avg_cpu_burst'] = round(avgBurst,2)
-    result['avg_wait'] = round(avgWait,2)
-    result['avg_turnaround'] = round(avgTT,2)
+    result['avg_cpu_burst'] = round(avgBurst,1)
+    result['avg_wait'] = round(avgWait,1)
+    result['avg_turnaround'] = round(avgTT,1)
     result['total_ctx_switch'] = str(cSwitches)
     result['total_preemptions'] = 0
     return result
@@ -598,7 +598,7 @@ def shortest_remaining_time(processes, t_cs):
         waitTotal = waitTotal + p['io_time']
 
     # Calculates averages
-    avg_burst = round((burstTotal / burstCount), 2)
+    avg_burst = round((burstTotal / burstCount), 1)
     total_ctx_switch = int(burstCount + result['total_preemptions'])
     avg_wait = waitTotal / total_ctx_switch
 
@@ -607,7 +607,7 @@ def shortest_remaining_time(processes, t_cs):
     result['avg_cpu_burst'] = avg_burst
     result['total_ctx_switch'] = total_ctx_switch
     result['avg_wait'] = avg_wait
-    result['avg_turnaround'] = round(avg_burst + ((t_cs * total_ctx_switch)), 2)
+    result['avg_turnaround'] = round(avg_burst + ((t_cs * total_ctx_switch)), 1)
     return result
 
 # # # # #
@@ -741,9 +741,9 @@ def round_robin(processes, t_cs=8, t_slice=80, rr_add="END"):
     # Puts statistics into result dict
     result = newResult()
     result['name'] = 'RR'
-    result['avg_cpu_burst'] = round(avgBurst,2)
-    result['avg_wait'] = round(avgWait,2)
-    result['avg_turnaround'] = round(avgTT,2)
+    result['avg_cpu_burst'] = round(avgBurst,1)
+    result['avg_wait'] = round(avgWait,1)
+    result['avg_turnaround'] = round(avgTT,1)
     result['total_ctx_switch'] = str((cSwitches + total_preemptions))
     result['total_preemptions'] = str(int(total_preemptions))
     return result
